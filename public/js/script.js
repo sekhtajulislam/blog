239e7b8c2edbf343d59ac08452807f1961113bf0
sekhtajulislam <iamsekhtajulislam@gmail.com>
2026-04-18 21:33:51 +0530

    Made some changes on viewing page

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,7 @@
             let view=document.getElementById("view-container")
             data.forEach(user => {
                 let div=document.createElement('div');
-                div.innerHTML=`<h1><a href="/post/${user.id}">${user.title}</a></h1>
-                 <p> ${user.content} </p> 
-                 <h5>Written By ${user.author}</h5>`
+                div.innerHTML=`<h1><a href="/post/${user.id}">${user.title}</a></h1>`
                 view.appendChild(div)
             });
         }).catch(error=> console.log("Error:",error))
